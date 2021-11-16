ba98d2552ea47ede198dca73f2572131b5147142
Nouredine <in_lamamra@esi.dz>
2021-11-16 23:50:55 +0100

    generation of board

---
+++ Squadro/Assets/Scripts/PlateauScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateauScript : MonoBehaviour
+{
+    public GameObject[] player1 = new GameObject[5];
+    public GameObject[] player2 = new GameObject[5];
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
