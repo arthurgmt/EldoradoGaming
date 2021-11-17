cd8c85cf1854d4f76f2b4ed596d98a01b1da4aa6
arthurgmt <arthur.guillaumot@gmail.com>
2021-11-17 10:49:58 +0100

    update scripts

---
+++ Squadro/Assets/Scripts/InitPion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitPion : MonoBehaviour
+{
+
+    public int NbCase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CompareTag("Pion1") | CompareTag("Pion5") | CompareTag("Pion6") | CompareTag("Pion10")) { NbCase = 1; }
+        else if (CompareTag("Pion2") | CompareTag("Pion4") | CompareTag("Pion7") | CompareTag("Pion9")) { NbCase = 3; }
+        else { NbCase = 2; }
+        
+    }
+
+    
+}
--- Squadro/Assets/Scripts/InstantiationExample.cs
+++ Squadro/Assets/Scripts/InstantiationExample.cs
@@ -12,15 +12,15 @@ public class InstantiationExample : MonoBehaviour
     void Start()
     {
         // Instantiate at position (0, 0, 0) and zero rotation.
-        Instantiate(myPrefab, new Vector3(13.4f, 3, 31), Quaternion.identity);
-        Instantiate(myPrefab, new Vector3(6.6f, 3, 31), Quaternion.identity);
-        Instantiate(myPrefab, new Vector3(-0.4f, 3, 31), Quaternion.identity);
-        Instantiate(myPrefab, new Vector3(-7.6f, 3, 31), Quaternion.identity);
-        Instantiate(myPrefab, new Vector3(-14.6f, 3, 31), Quaternion.identity);
-        Instantiate(myPrefab, new Vector3(24, 3, 21.4f), Quaternion.Euler (0f, 90f, 0f));
-        Instantiate(myPrefab, new Vector3(24, 3, 14.4f), Quaternion.Euler (0f, 90f, 0f));
-        Instantiate(myPrefab, new Vector3(24, 3, 7.4f), Quaternion.Euler (0f, 90f, 0f));
-        Instantiate(myPrefab, new Vector3(24, 3, 0.4f), Quaternion.Euler (0f, 90f, 0f));
-        Instantiate(myPrefab, new Vector3(24, 3, -6.6f), Quaternion.Euler (0f, 90f, 0f));
+        Instantiate(myPrefab, new Vector3(13.4f, 3, 31), Quaternion.identity).tag = "Pion1";
+        Instantiate(myPrefab, new Vector3(6.6f, 3, 31), Quaternion.identity).tag = "Pion2";
+        Instantiate(myPrefab, new Vector3(-0.4f, 3, 31), Quaternion.identity).tag = "Pion3";
+        Instantiate(myPrefab, new Vector3(-7.6f, 3, 31), Quaternion.identity).tag = "Pion4";
+        Instantiate(myPrefab, new Vector3(-14.6f, 3, 31), Quaternion.identity).tag = "Pion5";
+        Instantiate(myPrefab, new Vector3(24, 3, 21.4f), Quaternion.Euler (0f, 90f, 0f)).tag = "Pion6";
+        Instantiate(myPrefab, new Vector3(24, 3, 14.4f), Quaternion.Euler (0f, 90f, 0f)).tag = "Pion7";
+        Instantiate(myPrefab, new Vector3(24, 3, 7.4f), Quaternion.Euler (0f, 90f, 0f)).tag = "Pion8";
+        Instantiate(myPrefab, new Vector3(24, 3, 0.4f), Quaternion.Euler (0f, 90f, 0f)).tag = "Pion9";
+        Instantiate(myPrefab, new Vector3(24, 3, -6.6f), Quaternion.Euler (0f, 90f, 0f)).tag = "Pion10";
     }
 }
