425e30e95c9a387a167eeab808277960b2db758e
yecagp <yecagpp@gmail.com>
2021-11-17 22:41:01 +0100

    [CAMERA] Rotating camera + Fixing light issues
    Possiblité de tourner la camera pour les joueurs dans le mode 2D

--- Squadro/Assets/main_cam.cs
+++ Squadro/Assets/main_cam.cs
@@ -4,26 +4,19 @@ using UnityEngine;
 
 public class main_cam : MonoBehaviour
 {
-    public float sensitivity=1;
-    private GameObject parent;
+    public float s = 0.5f;//sensitivity
     //deplacer la camera
-    public void move(Vector3 position)
-    {
-        this.transform.position = position;
-    }
     // Start is called before the first frame update
     void Start()
     {
-        // position absolue
-        move(new Vector3(-10.1387f, 18f, -5.157465f));
     }
 
     // Update is called once per frame
     void Update()
-    {/*
-        float rotateHorizontal = Input.GetAxis("Mouse X");
-        float rotateVertical = Input.GetAxis("Mouse Y");
-        this.transform.RotateAround(this.transform.position, -Vector3.up, rotateHorizontal * sensitivity); //use transform.Rotate(-transform.up * rotateHorizontal * sensitivity) instead if you dont want the camera to rotate around the player
-        this.transform.RotateAround(Vector3.zero, this.transform.right, rotateVertical * sensitivity);*/
+    {
+        if (Input.GetMouseButton(0))// si click gauche
+        {
+            transform.Rotate(new Vector3(0, 0, Input.GetAxis("Mouse X") * s));//on tourne le plateau 
+        }
     }
 }
