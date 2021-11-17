0ca96e30120f1db4c921c39eb8130267e6911db2
Nouredine <in_lamamra@esi.dz>
2021-11-18 00:27:04 +0100

    add the theory behind movement management

--- Squadro/Assets/Scripts/PionMovement.cs
+++ Squadro/Assets/Scripts/PionMovement.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class PionMovement : MonoBehaviour
 {
+    public int nbDeplacement;
+    private bool allerSimple = false; // voir si le pion est arrivé a fait sa rotation ou pas.
+    private int deplacement = 7;
+    public int player;
+    public float initialCoordinatesX,initialCoordinatesY,initialCoordinatesZ;
+    private PlateauScript p;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(p);
     }
 
     // Update is called once per frame
     void Update()
+    {
+       
+    }
+
+    void deplacerPion()
     {
         
     }
--- Squadro/Assets/Scripts/PlateauScript.cs
+++ Squadro/Assets/Scripts/PlateauScript.cs
@@ -6,15 +6,33 @@ public class PlateauScript : MonoBehaviour
 {
     public GameObject[] player1 = new GameObject[5];
     public GameObject[] player2 = new GameObject[5];
+    public Dictionary<int,GameObject[]> dictionary= new Dictionary<int,GameObject[]>();
+    public bool[,] plateau = new bool[7,7];
     // Start is called before the first frame update
     void Start()
     {
-
+        for(int i = 1; i < 6; i++)//faire l'initialisation du plateau.
+        {
+            plateau[i, 0] = true;
+            plateau[6, i] = true;
+        }
+        dictionary.Add(1, this.player1);
+        dictionary.Add(2, this.player2);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void changeCoordinates()
+    {
+        
+    }
+
+    Dictionary<int, GameObject[]> getDictionnary()
+    {
+        return dictionary;
     }
 }
