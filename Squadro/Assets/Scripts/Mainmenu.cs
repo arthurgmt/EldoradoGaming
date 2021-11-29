a33152dadce1b63d0ddb75ea01a4fb10fededf70
arthurgmt <arthur.guillaumot@gmail.com>
2021-11-29 21:51:49 +0100

    setting Menu

---
+++ Squadro/Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,50 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Mainmenu : MonoBehaviour
+{
+    public Button play;
+    public Button settings;
+    public Button quit;
+    public Button createGame;
+    public Button joinGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        play.gameObject.SetActive(false);
+        settings.gameObject.SetActive(false);
+        quit.gameObject.SetActive(false);
+        createGame.gameObject.SetActive(true);
+        joinGame.gameObject.SetActive(true);
+
+    }
+
+    public void Settings()
+    {
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void CreateGame()
+    {
+
+    }
+    public void JoinGame()
+    {
+
+    }
+}
