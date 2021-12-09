d4fca3bbafba5b4fb84a3b849269a3f3fc09101b
yecagp <yecagpp@gmail.com>
2021-12-09 23:01:09 +0100

    [Photon + Multiplayer] First approach
    Fait :
    Lobby + Loading + initiation
    remaining:
    Gestion des roles
    bug après avoir bouger un seul pion

---
+++ Squadro/Assets/SpawnPlayers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class SpawnPlayers : MonoBehaviour
+{
+
+    public GameObject[] player = new GameObject[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
