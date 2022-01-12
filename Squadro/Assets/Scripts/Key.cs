695c15bc21099a2b74700f5511aed792fc0dc226
yecagp <yecagpp@gmail.com>
2022-01-12 23:44:55 +0100

    fix bug in key

--- Squadro/Assets/Scripts/Key.cs
+++ Squadro/Assets/Scripts/Key.cs
@@ -23,8 +23,10 @@ public class Key : MonoBehaviour
         string input = key.text.Trim();
         var task = reference.Child(input).GetValueAsync();
         yield return new WaitUntil(predicate: () => task.IsCompleted);
-        if (task.Result.Exists) {
-            DataSaver.saveData<KeyData>(new KeyData { key = input }, "keyData");
+        DataSnapshot ds = task.Result;
+        if (ds.Exists && ds.GetValue(true).ToString().Contains("-")) {
+            //DataSaver.saveData<KeyData>(new KeyData { key = input }, "keyData");
+            reference.Child(input).SetValueAsync("+");
             SceneManager.LoadScene("OfficialScene");
         }
         else Debug.LogError("there is error");
